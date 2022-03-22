fb16fac35a89a3f9ac7cf53cb5e9250859e1ac40
Rupa09 <rupaanupati09@gmail.com>
2022-03-22 10:24:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// function fruto(apples, mangoes)
+// {
+
+// console.log(apples, mangoes);
+// const juicy = 'juice with ${apples} apples and ${mangoes} mangoes.';
+// return juicy;
+
+// }
+
+// const fruto2 = fruto(5, 3);
+// console.log(fruto2);
+
+//-----arrows-------
+
+
+// const calcAge3 = birthYeah => 2037 - birthYeah;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+//     const age = 2037 - birthYeah;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+//   }
+//  console.log(yearsUntilRetirement(1991, 'Jonas'));
+//  console.log(yearsUntilRetirement(1980, 'Bob'));
+  
+// const calcAge1 = birthYear => 2042 - birthYear;
+// const age1 = calcAge1(1999);
+// console.log(age1);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+// const age = 2042 - birthYear;
+// const retirement = 65 - age;
+// return '${firstName} retires in ${retirement} years';
+
+// }
+// console.log(yearsUntilRetirement('Manikanth', 32));
+// console.log(yearsUntilRetirement('chandrakanth', 36));
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+//  console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
